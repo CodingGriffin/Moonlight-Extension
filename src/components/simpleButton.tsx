98e340d963e2d84aaa1f9f362194838f88890c3b
mvp79-dev <broweqos302679@hotmail.com>
2024-10-20 16:57:21 -0300

    add spinner for export and search

--- src/components/simpleButton.tsx
+++ src/components/simpleButton.tsx
@@ -15,7 +15,9 @@ const SimpleButton: React.FC<SimpleButtonProps> = ({
     <>
       <div className="m-8">
         <button
-          className="w-full focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+          className={`w-full focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 
+            dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 
+            ${isDisable ? "cursor-wait" : "cursor-pointer"}`}
           onClick={clickHandle}
           disabled={isDisable}
         >
